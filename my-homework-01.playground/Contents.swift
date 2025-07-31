2764bc284ebbec52a15084f636f656945173aab5
Akhmed Shapiev <shapievahmed1994@gmail.com>
2025-07-31 23:55:08 +0300

    1-е домашнее задание

--- my-homework-01.playground/Contents.swift
+++ my-homework-01.playground/Contents.swift
@@ -1,8 +1,5 @@
 import UIKit
 
-var greeting = "Hello, playground"
-print(greeting)
-
 //Домашнее задание - Вывести строку с целым количеством часов минут и секунд
 
 var secondsTotal = 654321
@@ -10,7 +7,3 @@ var hours = secondsTotal / 3600
 var minutes = (secondsTotal - hours * 3600) / 60
 var seconds = secondsTotal - hours * 3600 - minutes * 60
 print("\(secondsTotal) секунд это \(hours) часов \(minutes) минут \(seconds) секунд")
-
-
-
-
